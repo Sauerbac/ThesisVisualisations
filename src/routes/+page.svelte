4587cfe9b2d28f7475484afcbe5c082494c613d7
Simon Auerbach <simon.auerbach.ext@siemens.com>
2024-03-26 18:13:27 +0100

    added video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-2/3 mx-auto">
+	<h1 class="w-3/4 mt-24 mx-auto font-semibold text-4xl text-center">
+		Visualisation of the progress of Nelder-Mead optimization algorithm for my masters thesis
+	</h1>
+	<div class="w-3/4 mx-auto p-4">
+		<video controls loop src="/mix_grid.mp4"><track kind="captions" /></video>
+	</div>
+</div>
