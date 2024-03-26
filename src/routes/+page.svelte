e497a7281914d8088d93eeabaf90a7c61bf27b0f
Sauerbac <simon.auerbach@ise.fraunhofer.de>
2024-03-26 21:16:29 +0100

    page header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import { base } from '$app/paths';
 </script>
 
+<svelte:head>
+	<title>Thesis: Optimization Visualisation</title>
+</svelte:head>
+
 <div class="p-2 md:w-3/4 lg:w-1/2 md:mx-auto">
 	<h1 class="m-4 text-center font-semibold text-xl md:text-3xl">
 		Visualisation of the progress of Nelder-Mead optimization algorithm for my masters thesis
