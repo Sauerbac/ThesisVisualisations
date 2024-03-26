f936018f040e20c22f7041e5addfd8df603b0b88
Simon Auerbach <simon.auerbach.ext@siemens.com>
2024-03-26 18:15:25 +0100

    correct path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div class="w-2/3 mx-auto">
 	<h1 class="w-3/4 mt-24 mx-auto font-semibold text-4xl text-center">
 		Visualisation of the progress of Nelder-Mead optimization algorithm for my masters thesis
 	</h1>
 	<div class="w-3/4 mx-auto p-4">
-		<video controls loop src="/mix_grid.mp4"><track kind="captions" /></video>
+		<video controls loop src="{base}/mix_grid.mp4"><track kind="captions" /></video>
 	</div>
 </div>
