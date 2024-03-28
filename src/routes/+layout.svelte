646ce116002e9fa0b871c128ba398f1b9a551184
Simon Auerbach <simon.auerbach.ext@siemens.com>
2024-03-28 13:59:43 +0100

    basename added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,24 @@
 	import '../app.css';
 
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 	$page.url.pathname;
 	$: console.log($page.url.pathname);
 </script>
+
+<svelte:head><title>Thesis: Optimizer Visualisation</title></svelte:head>
 
 <div class="flex flex-col h-dvh">
 	<div class="flex w-full border-b border-zinc-600 bg-zinc-200">
 		<div class="flex mx-auto font-semibold text-lg">
 			<a
-				href="/"
+				href="{base}/"
 				class="px-8 py-4 border-b-4 {$page.url.pathname === '/'
 					? 'border-teal-600'
 					: 'hover:border-zinc-400'}">Animation</a
 			>
 			<a
-				href="/explorer"
+				href="{base}/explorer"
 				class="px-8 py-4 border-b-4 {$page.url.pathname === '/explorer'
 					? 'border-teal-600'
 					: 'hover:border-zinc-400'}">Explorer</a
